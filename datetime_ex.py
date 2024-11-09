787639dc828c7a4cac8aabaadc876341bbffc02d
PankovskiyIM <63363659+DonRobot-all@users.noreply.github.com>
2024-11-09 21:45:41 +0300

    Add files via upload

--- datetime_ex.py
+++ datetime_ex.py
@@ -1,16 +1,79 @@
 import datetime
+import calendar
 
-first_date = '02.09.2025'
-second_date = '08.09.2025'
-# second_date = '02-September-2025'
-# third_date = '02 Sep, 25'
-first = datetime.datetime.strptime(first_date, '%d.%m.%Y').date()
-second = datetime.datetime.strptime(second_date, '%d.%m.%Y').date()
-count = 0
-while first <= second:
-    count += 1
-    first += datetime.timedelta(1)
-print(count)
+# date
+# time
+# datetime
+
+somedate = datetime.date(2012, 9, 1)
+print(somedate)
+
+today = datetime.date.today()
+print(today)
+
+print(f'Сегодня {today.year}, месяц {today.month}, {today.day}, день недели {today.weekday()}')
+
+
+timer = datetime.time()
+print(timer)
+
+timer = datetime.time(16, 25)
+print(timer)
+
+timer = datetime.time(16,25,45)
+print(timer)
+
+
+day = datetime.datetime(2023, 12, 10, 12,15,15)
+print(day)
+
+today = datetime.datetime.now()
+print(today)
+print(today.date())
+print(today.time())
+print(today.day, today.minute)
+
+
+test = '22.07.2012'
+str_to_obj = datetime.datetime.strptime(test, '%d.%m.%Y')
+print(str_to_obj)
+
+test = '10.11.2024 12:30:30'
+str_to_obj = datetime.datetime.strptime(test, '%d.%m.%Y %H:%M:%S')
+# str_to_obj += datetime.timedelta(days=1)
+print(str_to_obj.weekday())
+print(str_to_obj.day)
+print(str_to_obj.month)
+
+
+    # %d: день месяца в виде числа
+    # %m: порядковый номер месяца
+    # %y: год в виде 2-х чисел
+    # %Y: год в виде 4-х чисел
+    # %H: час в 24-х часовом формате
+    # %M: минута
+    # %S: секунда
+
+
+
+
+
+
+
+
+
+
+# first_date = '02.09.2025'
+# second_date = '08.09.2025'
+# # second_date = '02-September-2025'
+# # third_date = '02 Sep, 25'
+# first = datetime.datetime.strptime(first_date, '%d.%m.%Y').date()
+# second = datetime.datetime.strptime(second_date, '%d.%m.%Y').date()
+# count = 0
+# while first <= second:
+#     count += 1
+#     first += datetime.timedelta(1)
+# print(count)
 # all_time = second - first
 # str_all_time = all_time.days
 # print(str_all_time)
--- task_time.py
+++ task_time.py
@@ -1,35 +1,27 @@
 import datetime
-# import calendar
+import calendar
 
-# def get_working_days(start_date, end_date, holidays):
-#     ...
-
-# def main():
-#     holidays = [
-#         datetime.date(2024, 11, 4)
-#     ]
-#     vacations = [
-#         ('02.09.2025', '09.09.2025')
-#     ]
-
-# main()
-
-def working_days(start, end, holidays):
+def get_working_days(start_date, end_date, holidays):
     count = 0
-    while start <= end:
-        if holidays == start:
-            start += datetime.timedelta(1)
-            continue
-        else:
+    while start_date <= end_date:
+        if start_date not in holidays:
             count += 1
-            start += datetime.timedelta(1)
-    return count
+    
 
-        
+        # if start_date.weekday() < 5: #and start_date not in holidays:
+        #     count += 1
+        # start_date += datetime.timedelta(days=1)
 
-first_date = '02.11.2024'
-second_date = '10.11.2024'
-first = datetime.datetime.strptime(first_date, '%d.%m.%Y').date()
-second = datetime.datetime.strptime(second_date, '%d.%m.%Y').date()
-days = working_days(first, second, datetime.date(2024, 11, 4))
-print(days)
+def main():
+    holidays = [
+        datetime.date(2024, 11, 4)
+    ]
+    vacations = [
+        ('02.09.2025', '09.09.2025')
+    ]
+    # тут ввод данных и вызов функции get_working_days
+    first = datetime.datetime.strptime(input(), '%d.%m.%Y').date()
+    second = datetime.datetime.strptime(input(), '%d.%m.%Y').date()
+    get_working_days(first, second, holidays)
+
+main()
